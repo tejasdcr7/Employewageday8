16ccc8f4f8b6fcec43fbf3a8d291cd186d384a2a
tejasdcr7 <tejasdandge@gmail.com>
2021-12-14 21:29:23 +0530

    added uc04

--- EmployeWageday8.java
+++ EmployeWageday8.java
@@ -3,23 +3,24 @@ package com.oops.wage;
 public class EmployeWageday8 {
     public static void main(String[] args) {
 
-        int fulltime = 1;
-        int parttime = 2;
+        final int fulltime=1;
+        final int partime=2;
         int Rate=20;
         int Emphour=0;
         int Empwage=0;
 
 
-        double empcheck = Math.floor(Math.random() * 10) % 3;
-        if (empcheck == fulltime)
-            Emphour=8;
-        else if (empcheck == parttime)
-            Emphour=4;
-        else
-            Emphour=0;
+        int empcheck =(int) Math.floor(Math.random() * 10) % 3;
+        switch (empcheck) {
+            case fulltime:
+                Emphour = 8;
+                break;
 
-
-        Empwage=Emphour*Rate;
+            case partime:
+                Emphour = 4;
+                break;
+        }
+        Empwage = Emphour*Rate;
         System.out.println("Empwage"+Empwage);
     }
 
