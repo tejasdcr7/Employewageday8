a3de898caa7da10bc00d544b552a3866d6b0cd5b
tejasdcr7 <tejasdandge@gmail.com>
2021-12-14 18:39:41 +0530

    added uc03

--- EmployeWageday8.java
+++ EmployeWageday8.java
@@ -4,18 +4,20 @@ public class EmployeWageday8 {
     public static void main(String[] args) {
 
         int fulltime = 1;
+        int parttime = 2;
         int Rate=20;
         int Emphour=0;
         int Empwage=0;
 
-        double empcheck = Math.floor(Math.random() * 10) % 2;
-        if (empcheck == fulltime) {
-            System.out.println("Emp is Present");
+
+        double empcheck = Math.floor(Math.random() * 10) % 3;
+        if (empcheck == fulltime)
             Emphour=8;
-        }
-        else{
-            System.out.println("Emp is absent");
-            Emphour=0;}
+        else if (empcheck == parttime)
+            Emphour=4;
+        else
+            Emphour=0;
+
 
         Empwage=Emphour*Rate;
         System.out.println("Empwage"+Empwage);
