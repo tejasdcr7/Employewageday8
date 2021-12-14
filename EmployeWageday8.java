8d32627405e4df374c162e0cc942147812065b28
tejasdcr7 <tejasdandge@gmail.com>
2021-12-14 18:36:34 +0530

    added uc02

--- EmployeWageday8.java
+++ EmployeWageday8.java
@@ -3,13 +3,24 @@ package com.oops.wage;
 public class EmployeWageday8 {
     public static void main(String[] args) {
 
-
         int fulltime = 1;
+        int Rate=20;
+        int Emphour=0;
+        int Empwage=0;
+
         double empcheck = Math.floor(Math.random() * 10) % 2;
         if (empcheck == fulltime) {
             System.out.println("Emp is Present");
-        } else
+            Emphour=8;
+        }
+        else{
             System.out.println("Emp is absent");
+            Emphour=0;}
+
+        Empwage=Emphour*Rate;
+        System.out.println("Empwage"+Empwage);
     }
 
-}
+
+    }
+
