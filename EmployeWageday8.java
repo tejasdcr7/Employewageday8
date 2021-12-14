aaf71ee3d0c8933324964b335a30fa8d80204d21
tejasdcr7 <tejasdandge@gmail.com>
2021-12-14 21:32:28 +0530

    added uc6

--- EmployeWageday8.java
+++ EmployeWageday8.java
@@ -6,12 +6,15 @@ public class EmployeWageday8 {
         final int fulltime=1;
         final int partime=2;
         final int mothworkdays=20;
+        final int totalworkinghours=100;
         int Rate=20;
         int Emphour=0;
         int Empwage=0;
         int totalmonthwage=0;
+        int day = 0;
+        while(day<mothworkdays && Emphour<totalworkinghours){
+            day++;
 
-        for(int day=1;day<mothworkdays;day++){
             int empcheck = (int) Math.floor(Math.random() * 10) % 3;
             switch (empcheck) {
                 case fulltime:
@@ -23,8 +26,8 @@ public class EmployeWageday8 {
                     break;
             }
             Empwage = Emphour*Rate;
-            totalmonthwage=totalmonthwage+Empwage;
-            System.out.println("Empwage"+Empwage);
+            totalmonthwage = totalmonthwage+Empwage;
+            System.out.println("Day" +day +"Emphours"+Emphour);
         }
         System.out.println("Totalmonthwage" +totalmonthwage);
     }
